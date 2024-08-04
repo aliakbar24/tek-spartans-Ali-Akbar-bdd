824bf6dc12bae6410f89703b0c4d20d10e064424
aliakbar24 <ali.akbar@tekschool.us>
2024-08-04 11:01:00 -0500

    make code maintainable

--- src/test/java/tek/bdd/utility/SeleniumUtility.java
+++ src/test/java/tek/bdd/utility/SeleniumUtility.java
@@ -2,6 +2,7 @@ package tek.bdd.utility;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -14,13 +15,22 @@ public class SeleniumUtility extends BaseSetup {
     private WebDriverWait getWait() {
         return new WebDriverWait(getDriver(), Duration.ofSeconds(10));
     }
-        //create a method to click on a given locator
-        public void clickOnElement(By locator) {
-            getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
+
+    private WebElement waitForVisibility(By locator) {
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+    //create a method to click on a given locator
+    public void clickOnElement(By locator) {
+        getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
 
     }
 
     public void sendText(By locator, String value) {
-            getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(value);
+            waitForVisibility(locator).sendKeys(value);
+    }
+
+    //Create method for getting the text of a locator
+    public String getElementText(By locator) {
+        return waitForVisibility(locator).getText();
     }
 }
