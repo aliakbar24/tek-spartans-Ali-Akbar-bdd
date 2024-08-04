7e5616f49bfe471999c8acf5b9ea402286a72392
aliakbar24 <ali.akbar@tekschool.us>
2024-08-04 10:53:29 -0500

    created selenium utility class

---
+++ src/test/java/tek/bdd/utility/SeleniumUtility.java
@@ -0,0 +1,26 @@
+package tek.bdd.utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import tek.bdd.base.BaseSetup;
+
+import java.time.Duration;
+
+public class SeleniumUtility extends BaseSetup {
+
+    private WebDriverWait getWait() {
+        return new WebDriverWait((getDriver(), Duration.ofSeconds(10));
+    }
+        //create a method to click on a given locator
+        public void clickOnElement(By locator) {
+            getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
+
+    }
+
+    public void sendText(By locator, String value) {
+            getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(value);
+    }
+}
