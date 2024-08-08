086264b386e5269de774471284e4fdf2c56770c7
aliakbar24 <ali.akbar@tekschool.us>
2024-08-08 12:29:50 -0500

    implement configs, cross browsing, and headless

--- src/test/java/tek/bdd/base/BaseSetup.java
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -2,22 +2,69 @@ package tek.bdd.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public abstract class BaseSetup {
 
     //Encapsulating driver instance
     private static WebDriver driver;
+    private final Properties properties;
+
+    //Find the full path to file
+    //FileInputStream to read the file
+    //Properties and load the FileInputStream to the properties.
+    public BaseSetup() {
+        //System.getProperty("user.dir") return the locator of your project
+        try {
+            String configFilePath = System.getProperty("user.dir") +
+                    "/src/test/resources/configs/dev-config.properties";
+            File file = new File(configFilePath);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (IOException ex) {
+            throw new RuntimeException("Something is wrong with Config file", ex);
+        }
+    }
+
     public void setupBrowser() {
-        /*
-        Web Driver instance
-        Navigation to retail
-        maximize
-        and implicit wait
-         */
-        driver = new ChromeDriver();
-        driver.get("https://retail.tekschool-students.com/");
+        //To open Chrome browser in headless mode
+        String browserType = properties.getProperty("ui.browser");
+        boolean isHeadless = Boolean.parseBoolean(properties.getProperty("ui.browser.headless"));
+
+        if(browserType.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            if (isHeadless)
+                options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        }else if (browserType.equalsIgnoreCase("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            if (isHeadless) options.addArguments("--headless");
+            driver = new EdgeDriver(options);
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            if (isHeadless)
+                options.addArguments("--headless");
+            driver = new FirefoxDriver();
+        }else {
+            throw new RuntimeException("Wrong browser type choose between chrome, firefox, or edge");
+        }
+
+
+
+
+        String url = properties.getProperty("ui.url");
+        driver.get(url);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
@@ -27,7 +74,6 @@ public abstract class BaseSetup {
         if (driver != null) {
             driver.quit();
         }
-
     }
 
     //Giving read-only indirect access to driver.
